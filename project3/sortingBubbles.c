31dafab1a9bbdf8eaecf39aa4bda82eae913759e
maxf-cs-bsu <maxcfleck@gmail.com>
2026-09-09 16:42:08 -0400

    Update sortingBubbles.c with starting code given

--- project3/sortingBubbles.c
+++ project3/sortingBubbles.c
@@ -1,1 +1,26 @@
 // Placeholder
+#include <stdio.h>
+const int MAX=9;
+
+void printValues(int*); // MAKE PRINT CODE
+void sort(int*); // MAKE SORT CODE
+void swap(int*, int*); // MAKE SWAP CODE (use splat @ value)
+
+int main(){
+  int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
+  printf("Before: \n");
+  printValues(values);
+
+  // test swap
+  int x = 3;
+  int y = 5;
+  printf("x: %d, y: %d \n", x, y);
+  swap(&x, &y);
+  printf("x: %d, y: %d \n", x, y);
+
+  sort(values);
+  printf("After: \n");
+  printValues(values);
+
+  return(0);
+} // end main
